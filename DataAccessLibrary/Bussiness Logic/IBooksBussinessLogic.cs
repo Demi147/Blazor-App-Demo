5b12380d5fe5ae1399964d0121cfd22c9f842e62
Demi147 <productionsDemigod@gmail.com>
2020-03-17 00:33:14 +0200

    1

---
+++ DataAccessLibrary/Bussiness Logic/IBooksBussinessLogic.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Bussiness_Logic
+{
+    public interface IBooksBussinessLogic
+    {
+        Task<BookTest> GetSingleTestBook(string _title);
+        Task<List<BookTest>> GetTestData();
+    }
+}
---
+++ DataAccessLibrary/Models/BookTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    public class BookTest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
