9f3182a4e54d24a1f33677351f2d2ea4c7ad0e97
Demi147 <productionsDemigod@gmail.com>
2020-03-21 16:24:43 +0200

    BusinessLogic_1

---
+++ DataAccessLibrary/Bussiness Logic/LoginLogic/LoginLogic.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLibrary.Models;
+using System.Linq;
+
+namespace DataAccessLibrary.Bussiness_Logic.LoginLogic
+{
+
+
+    public class LoginLogic
+    {
+        //Test list for login data
+        LoginTest[] x = new  LoginTest[]{ new LoginTest {ID=0,Name="Joey",Password="How you doin" },
+        new LoginTest{ID=1,Name="Test",Password="Test" }
+        };
+
+
+
+
+        //Actual logic
+
+        //Check Login Data
+        public Task<bool> CheckLogin()
+        {
+            return Task.FromResult(false);
+        }
+        public Task<bool> CheckLogin(LoginTest model)
+        {
+            if (x.Contains(model))
+            {
+                return Task.FromResult(true);
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+        }
+        public Task<bool> CheckLogin(string _userName,string _password)
+        {
+            //Get the list of matching Login data From Franco
+            // var x = dataAcces.getlogindata(_username);
+            //Check if it contains the username -- if return null or zero entries > wrong username
+            //if x == null return false;
+            //if x.count == 0 return false;
+
+
+            // if x[i].passowrd = _password return true else return false
+
+
+            //Test code 
+            foreach (var item in x)
+            {
+                if (item.Name==_userName)
+                {
+                    if (item.Password==_password)
+                    {
+                        return Task.FromResult(true);
+                    }
+                }
+            }
+            return Task.FromResult(false);
+
+        }
+        //Save new Login Data
+        //Get login data for user from ID
+    }
+}
---
+++ DataAccessLibrary/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary
+{
+    public static class Utilities
+    {
+        //Check if generic list contains an item
+        public static bool CheckIfContains<T>(List<T> _list,T _item) where T:TestInterface
+        {
+
+            return false;
+        }
+    }
+
+    public interface TestInterface
+    {
+        
+    }
+}
