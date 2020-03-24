9af7f0b7ed1ff5c23891251e1f7f025a82bb18dd
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-24 20:20:34 +0200

    Create Exchange.cs

---
+++ DataAccessLibrary/Models/Exchange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    public class Exchange
+    {
+        public int ExchangeID { get; set; }
+        public int UserID { get; set;}
+        public int SalesNumber { get; set; }
+
+    }
+}
