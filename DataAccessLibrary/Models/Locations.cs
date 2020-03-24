a1f687957765853a02d67d7b54e3848e304c19ef
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-24 20:20:31 +0200

    Create Locations.cs

---
+++ DataAccessLibrary/Models/Locations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    public class Locations
+    {   
+        public int BookNumber { get; set; }
+        public string BookInstitute { get; set; }
+        public string BookProvince { get; set; }
+        public string BookCity { get; set; }
+        public string BookSaleLocation { get; set; }
+    }
+}
