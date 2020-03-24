6dd0042ed7017c41e26f4d2beb4dc998e9ec5a14
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-24 20:20:29 +0200

    Create Books.cs

---
+++ DataAccessLibrary/Models/Books.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    public class Books
+    {
+        public int SalesNumber { get; set; }
+        public int UserID { get; set; }
+        public string BookTitle { get; set; }
+        public int BookEdition { get; set; }
+        public double BookPrice { get; set; }
+        public int BookNumber { get; set; }
+        public string ModuleCode { get; set; }
+
+    }
+}
