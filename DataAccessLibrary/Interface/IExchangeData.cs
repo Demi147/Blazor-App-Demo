941e5f0320319a598d9a2ceefd184990979d99e4
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-27 08:39:01 +0200

    Create IExchangeData.cs

---
+++ DataAccessLibrary/Interface/IExchangeData.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public interface IExchangeData
+    {
+        Task<List<Exchange>> GetExchanges();
+        Task InsertExchange(Exchange exchange);
+    }
+}
