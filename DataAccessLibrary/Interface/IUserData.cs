dcbd738f3e52e1945e5e57eff5cf1c9a15759e0d
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-27 08:39:09 +0200

    Create IUserData.cs

---
+++ DataAccessLibrary/Interface/IUserData.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public interface IUserData
+    {
+        Task<List<Users>> GetUsers();
+        Task InsertUser(Users user);
+    }
+}
