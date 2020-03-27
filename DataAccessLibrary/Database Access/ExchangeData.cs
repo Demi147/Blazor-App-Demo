0d7a1ce3adc0359c7bb464821889ddfd07363aae
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-27 08:38:51 +0200

    Create ExchangeData.cs

---
+++ DataAccessLibrary/Database Access/ExchangeData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLibrary.Models;
+
+namespace DataAccessLibrary
+{
+    public class ExchangeData : IExchangeData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public ExchangeData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<Exchange>> GetExchanges()
+        {
+            string sql = "Select * from dbo.tblExchange";
+
+            return _db.LoadData<Exchange, dynamic>(sql, new { });
+
+        }
+        public Task InsertExchange(Exchange exchange)
+        {
+            string sql = @"Insert into dbo.tblExchange(ExchangeID,UserID,SalesNumber)
+                              values(@ExchangeID,@UserID,@SalesNumber)";
+
+            return _db.SaveData(sql, exchange);
+        }
+    }
+
+}
+ 
