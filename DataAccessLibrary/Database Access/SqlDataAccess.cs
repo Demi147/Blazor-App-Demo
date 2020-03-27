e68fcd576b53d9e0442a23cf837cae4c1fb92ea2
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-27 08:38:55 +0200

    Create SqlDataAccess.cs

---
+++ DataAccessLibrary/Database Access/SqlDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+using System.Linq;
+
+namespace DataAccessLibrary
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public string ConnectionString { get; set; } = "Default";
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters)
+        {
+            string connectionstring = _config.GetConnectionString(ConnectionString);
+
+            using (IDbConnection connection = new SqlConnection(connectionstring))
+            {
+                var data = await connection.QueryAsync<T>(sql, parameters);
+
+                return data.ToList();
+            }
+        }
+
+        public async Task SaveData<T>(string sql, T parameters)
+        {
+            string connectionstring = _config.GetConnectionString(ConnectionString);
+
+            using (IDbConnection connection = new SqlConnection(connectionstring))
+            {
+                await connection.ExecuteAsync(sql, parameters);
+            }
+        }
+    }
+}
