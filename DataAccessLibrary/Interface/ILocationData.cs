b584338dc83982969fe723f7b310ed5b893c3a89
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-27 08:39:03 +0200

    Create ILocationData.cs

---
+++ DataAccessLibrary/Interface/ILocationData.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public interface ILocationData
+    {
+        Task<List<Locations>> GetLocations();
+        Task InsertLocation(Locations location);
+    }
+}
