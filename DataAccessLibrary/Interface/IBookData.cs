d07be50693578a7de41546b38ab9561112638d1e
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-27 08:38:59 +0200

    Create IBookData.cs

---
+++ DataAccessLibrary/Interface/IBookData.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public interface IBookData
+    {
+        Task<List<Books>> GetBooks();
+        Task InsertBook(Books book);
+    }
+}
