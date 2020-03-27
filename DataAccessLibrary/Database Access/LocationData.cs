f35fe3e809cc717f1b7cbe5a24baf9cbf1cc8221
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-27 08:38:53 +0200

    Create LocationData.cs

---
+++ DataAccessLibrary/Database Access/LocationData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLibrary.Models;
+
+namespace DataAccessLibrary
+{
+    public class LocationData : ILocationData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public LocationData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<Locations>> GetLocations()
+        {
+            string sql = "Select * from db.tblLocations";
+
+            return _db.LoadData<Locations, dynamic>(sql, new { });
+        }
+
+        public Task InsertLocation(Locations location)
+        {
+            string sql = @"Insert into dbo.tblLocations values(LocationsID,Province,City,Address,Building)
+                          values(@LocationsID,@Province,@City,@Address,@Building)";
+
+            return _db.SaveData(sql, location);
+        }
+    }
+}
