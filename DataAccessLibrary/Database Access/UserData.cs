fef564a6bea4d853b380ff64d0fe4fb6ebfee705
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-27 08:38:57 +0200

    Create UserData.cs

---
+++ DataAccessLibrary/Database Access/UserData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLibrary.Models;
+
+namespace DataAccessLibrary
+{
+    public class UserData : IUserData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public UserData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<Users>> GetUsers()
+        {
+            string sql = "Select * from dbo.tblUsers";
+
+            return _db.LoadData<Users, dynamic>(sql, new { });
+        }
+
+        public Task InsertUser(Users user)
+        {
+            string sql = @"Insert into dbo.tblUsers(UserID,UserName,UserEmail,UserPassword,IsAdmin)
+                          values(@UserID,@UserName,@UserEmail,@UserPassword,@IsAdmin)";
+
+            return _db.SaveData(sql, user);
+        }
+    }
+}
