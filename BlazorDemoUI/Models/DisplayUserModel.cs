3323bb5b073cf3f4ad63804f40ffa852fbfb1f3e
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-27 08:38:42 +0200

    Create DisplayUserModel.cs

---
+++ BlazorDemoUI/Models/DisplayUserModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDemoUI.Models
+{
+    public class DisplayUserModel
+    {
+        public int UserID { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Name is to long")]
+        [MinLength(1, ErrorMessage = "Name is to short")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string UserEmail { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Password is to long")]
+        [MinLength(6, ErrorMessage = "Password is to short")]
+        public string UserPassword { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
