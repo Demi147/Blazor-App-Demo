5d7baaec756eecfa3505650a57ff3059663a27af
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-31 19:54:00 +0200

    Update ISqlDataAccess.cs

--- DataAccessLibrary/Interface/ISqlDataAccess.cs
+++ DataAccessLibrary/Interface/ISqlDataAccess.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace DataAccessLibrary
@@ -7,6 +9,8 @@ namespace DataAccessLibrary
     {
         string ConnectionString { get; set; }
 
+        T Get<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        List<T> GetAll<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
         Task<List<T>> LoadData<T, U>(string sql, U parameters);
         Task SaveData<T>(string sql, T parameters);
     }
