d7e27804185126c11f10153e99018a02e5871dbb
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-31 19:53:58 +0200

    Update IBookData.cs

--- DataAccessLibrary/Interface/IBookData.cs
+++ DataAccessLibrary/Interface/IBookData.cs
@@ -6,12 +6,14 @@ namespace DataAccessLibrary
 {
     public interface IBookData
     {
+        Task<int> Count();
         Task<List<Books>> GetBooks();
         Task<List<Books>> GetSingleBook(int iSalesNumber);
-        Task<List<Books>> SearchBooks_Title(string sBookTitle);
-        Task<List<Books>> SearchBooks_ModuleCode(string sBookModuleCode);
-        Task<List<Books>> SearchBooks_Institue(string sBookInsitute);
         Task InsertBook(Books book);
+        Task<List<Books>> ListAll(int skip, int take, string orderby, string direction = "DESC");
         Task RemoveBook(Books book, int iSalesID);
+        Task<List<Books>> SearchBooks_Institue(string sBookInsitute);
+        Task<List<Books>> SearchBooks_ModuleCode(string sBookModuleCode);
+        Task<List<Books>> SearchBooks_Title(string sBookTitle);
     }
 }
