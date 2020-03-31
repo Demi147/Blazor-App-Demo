fc785a30227a52bb193cb42a71565c3c2f184666
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-31 19:53:43 +0200

    Create IFileUpload.cs

---
+++ BlazorDemoUI/Services/IFileUpload.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorInputFile;
+
+namespace BlazorDemoUI.Services
+{
+    public interface IFileUpload
+    {
+        Task UploadAsync(IFileListEntry file);
+    }
+}
