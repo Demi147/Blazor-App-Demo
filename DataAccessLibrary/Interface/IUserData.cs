20647f5eeb3ac01d42d6006a95d815663a6042ab
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-03-31 19:54:02 +0200

    Update IUserData.cs

--- DataAccessLibrary/Interface/IUserData.cs
+++ DataAccessLibrary/Interface/IUserData.cs
@@ -6,10 +6,12 @@ namespace DataAccessLibrary
 {
     public interface IUserData
     {
+        Task<int> Count();
         Task<List<Users>> GetAllUsers();
         Task<List<Users>> GetSingleUser(int iUserID);
         Task<List<Users>> GetUser_Email(string sUserEmail);
         Task InsertUser(Users user);
+        Task<List<Users>> ListAll(int skip, int take, string orderby, string direction = "DESC");
         Task RemoveUser(Users user);
     }
 }
