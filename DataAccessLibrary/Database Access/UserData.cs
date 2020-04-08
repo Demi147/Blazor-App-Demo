dadc7218440f61f4b3ec113089216cf5570b4448
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-04-08 11:43:08 +0200

    Update UserData.cs

--- DataAccessLibrary/Database Access/UserData.cs
+++ DataAccessLibrary/Database Access/UserData.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using DataAccessLibrary.Models;
 using System.Data;
+using Dapper;
 
 namespace DataAccessLibrary
 {
@@ -16,31 +17,57 @@ namespace DataAccessLibrary
             _db = db;
         }
 
-        public Task<List<Users>> GetAllUsers()
+        //##############################################################################################################################################################################################################
+        // Get methods for User Data Model
+        //##############################################################################################################################################################################################################
+        public Task<List<Users>> Get_AllUsers()
         {
-            string sql = "Select * from dbo.tblUsers";
+            string sql = @"Select * From dbo.tblUsers";
 
             return _db.LoadData<Users, dynamic>(sql, new { });
         }
 
-        public Task<List<Users>> GetSingleUser(int iUserID)
+        public Task<List<Users>> Get_SingleUser(int iUserID)
         {
-            string sql = "Select * from dbo.tblUsers where UserID =" + iUserID + "";
+            string sql = @"Select * From dbo.tblUsers Where UserID =" + iUserID + "";
 
             return _db.LoadData<Users, dynamic>(sql, new { });
         }
 
-
-        public Task<List<Users>> GetUser_Email(string sUserEmail)
+        public Task<List<Users>> Get_UserID_Email(string sUserEmail)
         {
-            string sql = "Select * from dbo.tblUsers where UserEmail ='" + sUserEmail + "'";
+            string sql = @"Select UserID From dbo.tblUsers Where UserEmail ='" + sUserEmail + "'";
 
             return _db.LoadData<Users, dynamic>(sql, new { });
         }
 
+        public Task<bool> Get_IsAdmin_UserID(int iUserID)//Method that returns true if the user is admin and false if not using the userID to find user int DB
+        {
+            var bAdmin = Task.FromResult(_db.Get<bool>($"Select IsAdmin From dbo.tblUsers Where UserID = " + iUserID + "", null, commandType: CommandType.Text));
+
+            return bAdmin;
+        }
+
+        public Task<string> Get_UserEmail_UserID(int iUserID)
+        {
+            var sEmail = Task.FromResult(_db.Get<string>($"Select UserEmail From dbo.tblUsers Where UserID = " + iUserID + "", null, commandType: CommandType.Text));
+
+            return sEmail;
+        }
+
+        public Task<string> Get_UserPassword_UserID(int iUserID)
+        {
+            var sPassword = Task.FromResult(_db.Get<string>($"Select UserPassword From dbo.tblUsers Where UserID = " + iUserID + "", null, commandType: CommandType.Text));
+
+            return sPassword;
+        }
+
+        //##############################################################################################################################################################################################################
+        // CRUD methods for User Data Model
+        //##############################################################################################################################################################################################################
         public Task InsertUser(Users user)
         {
-            string sql = @"Insert into dbo.tblUsers(UserID,UserName,UserEmail,UserPassword,IsAdmin)
+            string sql = @"Insert Into dbo.tblUsers(UserID,UserName,UserEmail,UserPassword,IsAdmin)
                           values(@UserID,@UserName,@UserEmail,@UserPassword,@IsAdmin)";
 
             return _db.SaveData(sql, user);
@@ -48,28 +75,39 @@ namespace DataAccessLibrary
 
         public Task RemoveUser(Users user)
         {
-            string sql = @"Delete from  dbo.tblUsers Where UserID =" + user.UserID + "";
+            string sql = @"Delete From  dbo.tblUsers Where UserID =" + user.UserID + "";
 
             return _db.SaveData(sql, user);
         }
 
-        //public Task UpdateUser(Users user)
+        //public Task<int> UpdateUser(Users user)
         //{
-        //    string sql =@"Update "
+        //    var dbPara = new DynamicParameters();
+        //    dbPara.Add("Id", user.UserID);
+        //    dbPara.Add("Name", user.UserName, DbType.String);
+
+        //    var updateUser = Task.FromResult(_db.Update<int>("[dbo].[tblUsers]", dbPara, commandType: CommandType.StoredProcedure));
+
+        //    return updateUser;
         //}
 
+        //##############################################################################################################################################################################################################
+        // Other methods for User Data Model
+        //##############################################################################################################################################################################################################
         public Task<int> Count()
         {
-            var totalUsers = Task.FromResult(_db.Get<int>("Select count(*) from dbo.tblUsers", null, commandType: System.Data.CommandType.Text));
+            var totalUsers = Task.FromResult(_db.Get<int>($"Select count(*) From dbo.tblUsers", null, commandType: System.Data.CommandType.Text));
 
             return totalUsers;
         }
 
         public Task<List<Users>> ListAll(int skip, int take, string orderby, string direction = "DESC")
         {
-            var users = Task.FromResult(_db.GetAll<Users>($"Select * from dbo.tblUsers Order By {orderby} {direction} Offset {skip} Rows fetch next {take} rows only;", null, commandType: CommandType.Text));
+            var users = Task.FromResult(_db.GetAll<Users>($"Select * From dbo.tblUsers Order By {orderby} {direction} Offset {skip} Rows fetch next {take} rows only;", null, commandType: CommandType.Text));
 
             return users;
         }
+
+
     }
 }
