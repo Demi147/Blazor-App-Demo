f6b184e1e4d152b6ebf6a56b90a9f3955670235e
CarmenLH <61046574+CarmenLH@users.noreply.github.com>
2020-04-08 15:07:01 +0200

    Create ILocationData.cs

---
+++ DataAccessLibrary/Database Access/ILocationData.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public interface ILocationData
+    {
+        Task<string> Get_Address_LocalID(int iLocationID);
+        Task<List<Locations>> Get_AllCities();
+        Task<List<Locations>> Get_AllLocations();
+        Task<List<Locations>> Get_AllProvince();
+        Task<int> Get_LocationID_Address(string SLocationAddress, string sCity);
+        Task InsertLocation(Locations location);
+        Task RemoveLocation(Locations location, int iLocationID);
+    }
+}
