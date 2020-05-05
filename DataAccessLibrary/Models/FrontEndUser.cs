7668eb3c2254823cf6a016587a8d79db74d4c338
Demi147 <productionsDemigod@gmail.com>
2020-05-05 12:28:38 +0200

    Random update

---
+++ DataAccessLibrary/Models/FrontEndUser.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLibrary.Models
+{
+    public class FrontEndUser
+    {
+        public int UserID { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string UserEmail { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
